240a86693d0e951b6398bc3175ce36a9980669e9
learning <learning@DIN66007845.corp.capgemini.com>
2018-07-10 12:38:32 +0530

    first1

--- paw/src/main/java/com/capgemini/paw/service/IPaymentAppService.java
+++ paw/src/main/java/com/capgemini/paw/service/IPaymentAppService.java
@@ -1,5 +1,17 @@
 package com.capgemini.paw.service;
 
+
+
+import com.capgemini.paw.bean.PaymentAppDetails;
+
 public interface IPaymentAppService {
 
+	public int CreateAccount(PaymentAppDetails paymentappdetails);
+	public PaymentAppDetails ShowBalance(int accountNo);
+	public void Deposit();
+	public void Withdraw();
+	public void FundTransfer();
+	//public Map<accountNo,PaymentAppDetails> PrintPassbook();
+
+	
 }
--- paw/src/main/java/com/capgemini/paw/ui/PaymentAppClient.java
+++ paw/src/main/java/com/capgemini/paw/ui/PaymentAppClient.java
@@ -2,4 +2,29 @@ package com.capgemini.paw.ui;
 
 public class PaymentAppClient {
 
-}
+	public static void main(String[] args) {
+		
+		int choice = -1;
+		while (choice != 6) {}
+
+			System.out.println("                                                     ");
+
+			System.out.println(".....................................................");
+			System.out.println("...................PRODUCT DETAILS...................");
+			System.out.println(".....................................................");
+			System.out.println("..                                                 ..");
+			System.out.println("..              1.Add Product Details              ..");
+			System.out.println("..              2.Display Details                  ..");
+			System.out.println("..              3.Update Product Details           ..");
+			System.out.println("..              4.Remove Product Details           ..");
+			System.out.println("..              5.Display All Product List         ..");
+			System.out.println("..              6.Exit                             ..");
+			System.out.println("..                                                 ..");
+			System.out.println(".....................................................");
+			System.out.println(".....................................................");
+			System.out.println("                                                     ");
+		
+	}
+	}
+	
+
